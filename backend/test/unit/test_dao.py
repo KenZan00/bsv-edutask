3be383c37dba1e9eb8f34132a1fba852a690c11b
KenZan00 <akerup.kenny@gmail.com>
2026-05-20 15:58:26 +0200

    Most correct, do not raise duplicate error on last assignment though

--- backend/test/unit/test_dao.py
+++ backend/test/unit/test_dao.py
@@ -1,27 +1,57 @@
 import pytest
 from unittest.mock import MagicMock
+from unittest.mock import patch
 from src.util.dao import DAO
+from pymongo.errors import WriteError
+from pymongo.errors import DuplicateKeyError
+
+
+validation_structure = { "$jsonSchema": {
+        "bsonType": "object",
+        "required": ["firstName", "lastName", "email"],
+        "properties": {
+            "firstName": {
+                "bsonType": "string",
+                "description": "the first name of a user must be determined"
+            }, 
+            "lastName": {
+                "bsonType": "string",
+                "description": "the last name of a user must be determined"
+            },
+            "email": {
+                "bsonType": "string",
+                "description": "the email address of a user must be determined",
+                "uniqueItems": True
+            },
+            "tasks": {
+                "bsonType": "array",
+                "items": {
+                    "bsonType": "objectId"
+                }
+            }
+        }
+    }}
 
 
 # As testers, we viewed this as a fully testable NONE production database.
 @pytest.fixture
 def db_sut():
-    database = DAO('user')
+    with patch("src.util.dao.getValidator") as mock_get_validator:
+        mock_get_validator.return_value = validation_structure
+        database = DAO('user')
 
-    # Mock validator into dao to not  create real side_effects from mongoDB
-    database.mocked_validator = MagicMock()
+        database.collection.delete_many({})
 
-    database.collection.delete_many({})
+        yield database
 
-    yield database
+        database.collection.delete_many({})
 
-    database.collection.delete_many({})
 
 # Test create document with a valid structure.
 @pytest.mark.integration
 def test_create_document_valid(db_sut):
-    # Mocks the validation with a fake validator that only return True.
-    db_sut.mocked_validator.validate.return_value = True
+
+
     document = {
         "firstName": "Testing",
         "lastName": "Testsson",
@@ -36,59 +66,47 @@ def test_create_document_valid(db_sut):
 
 # Missing required field, raise exception.
 @pytest.mark.integration
-def test_create_document_invalid(db_sut):
-    db_sut.mocked_validator.validate.side_effect = Exception("missing required")
+def test_create_document_not_required_input_email(db_sut):
+
     document = {
         "firstName": "Testing",
         "lastName": "Testsson",
         # No email
     }
     
-    with pytest.raises(Exception):
-        db_sut.mocked_validator.validate(document)
+    with pytest.raises(WriteError):
         db_sut.create(document)
 
 
-# # Testing wrong data format, integer instead of string as specified by validator
-# @pytest.mark.integration
-# def test_create_document_wrong_datatype(db_sut):
-#     document = {
-#         "firstName": "Testing",
-#         "lastName": "Testsson",
-#         "email": 757
-#     }
+# Testing wrong data format, integer instead of string as specified by validator
+@pytest.mark.integration
+def test_create_document_wrong_datatype(db_sut):
+    document = {
+        "firstName": "Testing",
+        "lastName": "Testsson",
+        "email": 757
+    }
     
-#     with pytest.raises(Exception):
-#         db_sut.create(document)
+    with pytest.raises(WriteError):
+        db_sut.create(document)
+
+# Testing duplciate unique item, which should raise an exception
+@pytest.mark.integration
+def test_create_document_unique_item_constraint(db_sut):
+    
+    document = {
+        "firstName": "Testing",
+        "lastName": "Testsson",
+        "email": "unique.email@test.com"
+    }
+
+    db_sut.create(document)
+    
+    with pytest.raises(DuplicateKeyError):
+        db_sut.create(document)
 
 
-# # Test with optional task-list
-# @pytest.mark.integration
-# def test_create_document_optinal_taks(db_sut):
-#     document = {
-#         "firstName": "Testing",
-#         "lastName": "Testsson",
-#         "email": "test2@test.com",
-#         "tasks": []
-#     }
 
-#     result = db_sut.create(document)
-
-#     assert result is not None
-#     assert "_id" in result
-
-# # Test with optional task-list as wrong format.
-# @pytest.mark.integration
-# def test_create_document_optinal_taks_wrong(db_sut):
-#     document = {
-#         "firstName": "A",
-#         "lastName": "B",
-#         "email": "a@test.com",
-#         "tasks": ["wrong"]
-#     }
-
-#     with pytest.raises(Exception):
-#         db_sut.create(document)
 
 
 # @pytest.mark.integration
